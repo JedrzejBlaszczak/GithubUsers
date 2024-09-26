accaf185d791fa2b3ab5f6b7e807add5c8b427e2
Jędrzej Błaszczak <jedrzejblaszczak@Jedrzejs-MacBook-Pro.local>
2024-09-26 18:33:14 +0200

    add compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,12 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
